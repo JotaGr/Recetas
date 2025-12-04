9f5055fd05f15df16164c0d79d01be54cb6e8b7e
JotaGr <115045195+JotaGr@users.noreply.github.com>
2025-12-04 16:45:40 -0300

    deploy GP

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/recetas2025/', // Base path para GitHub Pages
+  base: '/Recetas/', // Base path para GitHub Pages
   server: {
     open: true,
   },
